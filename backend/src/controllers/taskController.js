a96a65f5e46c1143cdfd2059fd732a07c4520766
AndrewFlowe-rs <arnaldomahone@gmail.com>
2025-04-01 17:11:46 -0300

    se avamza hasta actualizar una tarea

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,23 +1,39 @@
 let task = []; // Array para almacenar tareas
 
-
-// obtener las tareas 
+// obtener las tareas
 const getTask = (req, res) => {
-    res.jspn(task);
-}
+  res.jspn(task);
+};
 
 // crear una nueva tarea
-const createTask = (req,res) => {
-    const {title, description} = req.body;
+const createTask = (req, res) => {
+  const { title, description } = req.body;
 
-    if (!title) {
-        return res.estatus(400).json({ error: "el titulo es requerido"})    
+  if (!title) {
+    return res.estatus(400).json({ error: "el titulo es requerido" });
+  }
+
+  const newTask = {
+    id: Date.now(),
+    title,
+    description,
+    completed: false,
+  };
+  tasks.push(newTask);
+  task.push(newTask);
+  res.status(201).json(newTask);
+};
+
+
+//Actualizar tarea
+const updateTask = (req ,res) => {
+    const { id } = req.params;
+    const {title,description,completed} = req.body;
+
+    const taskIndex = task.findIndex((task) => task.id == id);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({ error: "Tarea no encontrada" });
     }
 
-    const newTask = {
-        id: Date.now(),title,description,completed: false,};
-        tasks.push(newTask);
-        task.push(newTask);
-         res.json(newTask);
-    }
-    
+}
