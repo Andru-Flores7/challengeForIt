85d941bef2b9b6b4faa14419c2815357eb2a785b
AndrewFlowe-rs <arnaldomahone@gmail.com>
2025-05-26 20:17:30 -0300

    termina

--- frontend/src/components/CreateTaskForm.jsx
+++ frontend/src/components/CreateTaskForm.jsx
@@ -1,58 +1,70 @@
-import React from "react";
-import axios from "axios";
-import { useState } from "react";
-import "../assets/css/CreateTaskForm.css";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-const CreateTaskForm = ({ onTaskCreated}) => {
+import axios from "axios";
+import "../assets/css/EditTaskForm.css";
+
+const CreateTaskForm = ({ onTaskCreated }) => {
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState(""); 
+  const [description, setDescription] = useState("");
   const [completed, setCompleted] = useState(false);
   const navigate = useNavigate();
-  const handleSubmit = async (e) =>{
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3000/Tasks", {
+      await axios.post("http://localhost:3000/Tasks", {
         title,
         description,
         completed,
       });
-    
-      console.log("Tarea creada con exito" ,response.data);
-      onTaskCreated && onTaskCreated();//actualizar la lista de tareas
-      setTitle("");//limpiar los campos
-      setDescription("");//limpiar los campos
+      onTaskCreated && onTaskCreated();
+      setTitle("");
+      setDescription("");
       setCompleted(false);
-      navigate("/tasks"); 
+      navigate("/tasks");
     } catch (error) {
-      console.error ("Error al crear la tarea" , error); 
-  }
-    
-  }
+      console.error("Error al crear la tarea", error);
+    }
+  };
+
   return (
     <>
-      <div className="create" style={{textDecoration: 'none' }}>
+      <div className="Edit">
         <Link to="/tasks" style={{ textDecoration: 'none' }}>
-          <h1 >Crear Nueva Tarea</h1>
+          <h1>Crear Nueva Tarea</h1>
         </Link>
       </div>
-      <div className="containerCreateForm">
-        <form className="unified-form" onSubmit={handleSubmit}>
+      <div className="containerEditForm">
+        <form onSubmit={handleSubmit} className="editForm">
           <div>
             <label htmlFor="title">Tarea</label>
-            <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
           <div>
-            <label htmlFor="description">Descripcion</label>
-            <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+            <label>Descripcion</label>
+            <textarea
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="completed">Estado</label>
-            <select id="completed" value={completed ? "completado" : "incompleto"} onChange={e => setCompleted(e.target.value === "completado") }>
+            <select
+              id="completed"
+              value={completed ? "completado" : "incompleto"}
+              onChange={e => setCompleted(e.target.value === "completado")}
+            >
               <option value="incompleto">Incompleto</option>
               <option value="completado">Completado</option>
             </select>
           </div>
-          <button className="btnCreate btn btn-primary" type="submit">
+          <button type="submit" className="btn btn-primary">
             Crear
           </button>
         </form>
